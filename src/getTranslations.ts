c8210b7b98daafb87223282e8d8b6daac0803005
Rodry <38259440+ImRodry@users.noreply.github.com>
2022-07-11 02:00:33 +0100

    refactor: change script a little and minor fixes

--- src/getTranslations.ts
+++ src/getTranslations.ts
@@ -2,23 +2,18 @@ import { writeFileSync } from "node:fs"
 
 import Ota from "@crowdin/ota-client"
 
-const otaClient = new Ota("d7578b29286a48bcaf7fec87zgb")
+const otaClient = new Ota("d7578b29286a48bcaf7fec87zgb");
 
-async function main() {
+(async function () {
 	const [translations, formattedLangs, languages] = await Promise.all([
 			otaClient.getTranslations(),
 			otaClient.getReplacedLanguages("%locale_with_underscore%"),
 			otaClient.listLanguages(),
 		]),
-		result: Record<string, { translation: string; url: string }> = {}
+		result: Record<string, string> = {}
 
-	Object.entries(translations)
-		.sort((a, b) => a[0].localeCompare(b[0]))
-		.map(([languageCode, translations], i) => {
-			result[formattedLangs[languages.indexOf(languageCode)]] = translations[0].content.language
-		})
+	for (const [languageCode, translation] of Object.entries(translations).sort(([a], [b]) => a.localeCompare(b)))
+		result[formattedLangs[languages.indexOf(languageCode)]] = translation[0].content.language
 
 	writeFileSync("./languageNames.json", JSON.stringify(result, null, "\t"))
-}
-
-main()
+})()
